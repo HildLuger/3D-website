8aee82d1284f48b23868f576a1b16579c57bc5fb
3js <3js.code@gmail.com>
2021-11-10 06:48:14 -0300

    first commit

---
+++ tex2.js
@@ -0,0 +1,7 @@
+var string = " <h1>The world’s leading agency supporting independent hotels.";
+var str = string.split("");
+var el = document.getElementById('str');
+(function animate() {
+str.length > 0 ? el.innerHTML += str.shift() : clearTimeout(running); 
+var running = setTimeout(animate, 90);
+})();
